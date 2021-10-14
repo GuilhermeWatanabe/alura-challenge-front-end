2bb070957a7a828de32d1904daa00770d4775aaf
Guilherme Watanabe <watanab430@gmail.com>
2021-10-14 19:23:48 -0300

    Feat: create the filled and outlined default buttons.

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { baseRadius, blue100, blue200, blue300, blue300RGB } from "./variables";
 
 /**
  * Typography
@@ -49,4 +50,52 @@ export const SideBarTitle = styled.span`
     text-transform: uppercase;
 
     ${sidebarTitleSize}
+`;
+
+export const FilledButton = styled.button`
+    background-color: ${blue300};
+    border: 0;
+    border-radius: ${baseRadius};
+    box-sizing: border-box;
+    color: #051D3B;
+    font-weight: 400;
+    height: 56px;
+    width: 100%;
+
+    &:hover {
+        background-color: ${blue200};
+    }
+
+    &:active {
+        border: 4px solid;
+        border-color: rgba(${blue300RGB}, 0.72);
+    }
+
+    &:focus {
+        background-color: ${blue100};
+    }
+`;
+
+export const OutlinedButton = styled.button`
+    background-color: rgba(${blue300RGB}, 0.08);
+    border: 0;
+    border-radius: ${baseRadius};
+    box-sizing: border-box;
+    color: white;
+    font-weight: 400;
+    height: 56px;
+    width: 100%;
+
+    &:hover {
+        background-color: rgba(${blue300RGB}, 0.16);
+    }
+
+    &:active {
+        border: 4px solid;
+        border-color: rgba(${blue300RGB}, 0.24);
+    }
+
+    &:focus {
+        background-color: rgba(${blue300RGB}, 0.24);
+    }
 `;
